73158a32816173321c1945f4aac13ea121b04cf3
Ruslan-Gubin <ruslan_gubin@rambler.ru>
2022-10-25 01:15:32 +0300

    test

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 import chalk from "chalk";
 import cors from "cors";
 import * as routes from "./routes/index.js";
-// import * as dotenv from 'dotenv'
-// dotenv.config()
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const PORT = process.env.PORT || 80
+// const PORT = process.env.PORT || 80
 
 const errorNsg = chalk.bgKeyword("white").redBright;
 const successNsg = chalk.bgKeyword("green").white;
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 // (async () => {
-  //  mongoose
-  //   .connect(process.env.MONGO_URL)
-  //   .then(() => console.log(successNsg("DB Product ok")))
-  //   .catch((err) => console.log(errorNsg("DB error", err)));
+   mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log(successNsg("DB Product ok")))
+    .catch((err) => console.log(errorNsg("DB error", err)));
   // })();
    
 //Routes
@@ -29,11 +29,42 @@ app.use("/uploads", express.static("uploads"));
   app.use(routes.apiAuthRoutes);
   app.use(routes.apiUploadRoutes);
   app.get('/test', (req, res) => {
-    res.end('<h1>About</h1>')
+    res.end(`
+    <div>
+    <nav>
+    <ul>
+    <li>
+    <a href='/test'>push</a>
+    </li>
+    <li>
+    <a href='/home'>home</a>
+    </li>
+    </ul>
+    </nav>
+    <h1>Test</h1>
+    </div>
+    `)
+  })
+  app.get('/home', (req, res) => {
+    res.end(`
+    <div>
+    <nav>
+    <ul>
+    <li>
+    <a href='/test'>push</a>
+    </li>
+    <li>
+    <a href='/home'>home</a>
+    </li>
+    </ul>
+    </nav>
+    <h1>Home</h1>
+    </div>
+    `)
   })
 
-  app.listen(PORT, (error) => {
-  // app.listen(process.env.PORT || 4444, (error) => {
+  // app.listen(PORT, (error) => {
+  app.listen(process.env.PORT || 4444, (error) => {
     error
       ? console.log(errorNsg(error))
       : console.log(successNsg(`Listening port ${process.env.PORT || 4444}`));
