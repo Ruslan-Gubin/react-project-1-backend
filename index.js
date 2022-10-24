875449504a7248ff528343dfc3780385251ac92c
Ruslan-Gubin <ruslan_gubin@rambler.ru>
2022-10-25 00:48:59 +0300

    test

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import chalk from "chalk";
 import cors from "cors";
-// import { MONGO_DB_PRODUCTS } from "./service/constants/namePassDb.js";
 import * as routes from "./routes/index.js";
-import * as dotenv from 'dotenv'
-dotenv.config()
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+
+const PORT = process.env.PORT || 80
 
 const errorNsg = chalk.bgKeyword("white").redBright;
 const successNsg = chalk.bgKeyword("green").white;
@@ -16,10 +17,10 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 // (async () => {
-   mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log(successNsg("DB Product ok")))
-    .catch((err) => console.log(errorNsg("DB error", err)));
+  //  mongoose
+  //   .connect(process.env.MONGO_URL)
+  //   .then(() => console.log(successNsg("DB Product ok")))
+  //   .catch((err) => console.log(errorNsg("DB error", err)));
   // })();
    
 //Routes
@@ -27,8 +28,12 @@ app.use("/uploads", express.static("uploads"));
   app.use(routes.apiPostRoutes);
   app.use(routes.apiAuthRoutes);
   app.use(routes.apiUploadRoutes);
+  app.get('/test', (req, res) => {
+    res.end('<h1>About</h1>')
+  })
 
-  app.listen(process.env.PORT || 4444, (error) => {
+  app.listen(PORT, (error) => {
+  // app.listen(process.env.PORT || 4444, (error) => {
     error
       ? console.log(errorNsg(error))
       : console.log(successNsg(`Listening port ${process.env.PORT || 4444}`));
