51f25436e3303adc9824fcfe59bcf89cdf28f118
Ruslan-Gubin <ruslan_gubin@rambler.ru>
2022-10-25 00:16:36 +0300

    heruku

--- routes/api-uploadRoutes.js
+++ routes/api-uploadRoutes.js
@@ -2,9 +2,13 @@ import { checkAuth } from "../service/checkAuth.js";
 import Router from 'express';
 import multer from 'multer';
 const router = new Router()
+import fs from 'fs';
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads')
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
